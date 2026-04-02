db5b068cc8a243c83741fcce4c583bd30991c5b4
Ngàn Thành Phú <nhocjoon123@gmail.com>
2026-04-02 15:59:16 +0700

    aa

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
-          <p>Hello World</p>
+          <p>Hello World!</p>
         </div>
         <button
           className="counter"
